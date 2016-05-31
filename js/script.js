a2d8ebb8e1708a6a4f221f10b1cc728fa5277742
Yuan <yuanji.yj@gmail.com>
2016-05-31 14:53:39 -0400

    added new files

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    // load streetview
+    
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+    console.log(address);
+    $greeting.text('So, you want to live at ' + address + '?');
+    
+    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address.replace(/ +/g, "");
+    console.log(streetviewUrl);
+    $body.append('<img class="bgimg" src=' + streetviewUrl + ' />');
+    
+    
+    
+    
+    
+    return false;
+};
+
+$('#form-container').submit(loadData);
